a349e799a8af659a904540ec20f646d4d6582589
ghkdtlwns987 <ghkdtlwns987@naver.com>
2023-04-26 02:08:32 +0900

    Auth Server v1

---
+++ src/main/java/com/amd/backend/Domain/User/Repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.amd.backend.Domain.User.Repository;
+
+import com.amd.backend.Domain.User.Entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUserId(String userId);
+    UserEntity findByEmail(String username);
+
+    boolean existsByEmail(String username);
+    boolean existsByName(String name);
+}
