29685118ef9e4025ccf4b083294cfad5ca0d64db
ghkdtlwns987 <ghkdtlwns987@naver.com>
2023-06-01 19:34:31 +0900

    push

--- src/main/java/com/amd/backend/Domain/User/Repository/AuthRepository.java
+++
@@ -1,8 +0,0 @@
-package com.amd.backend.Domain.User.Repository;
-
-import com.amd.backend.Domain.User.DTO.TokenDTO;
-import com.amd.backend.Domain.User.DTO.UserDTO;
-
-public interface AuthRepository {
-    void doLogout(String userId);
-}
